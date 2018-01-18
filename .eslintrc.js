714f95930e9f05135157dff06bae5322c7e84e62
xzc <1141957724@qq.com>
2018-01-18 18:50:36 +0800

    优化

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    //禁用var，用let和const代替
+    'no-var': 0,
+    //强制驼峰法命名
+    'camelcase':2,
+    'space-before-function-paren': [0,'always']
+  }
+}
